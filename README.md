839cce590cc07930185a3872853bb44df17dd72e
Troy Wolters <14ctw@queensu.ca>
2017-10-17 11:50:26 -0400

    Add installation script for libtins

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ELEC 498 Project
+
+## Installing libtins
+
+All of the commands needed for installing libtins have been put into the file `install.sh`. Just run:
+
+```shell
+chmod +x install.sh
+./install.sh
+```
+
+This has been tested on an Ubuntu-based Linux system.
+
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo -e "\nsudo apt-get install libpcap-dev libssl-dev cmake\n"
+sudo apt-get install libpcap-dev libssl-dev cmake
+
+echo -e "\nCloning libtins\n"
+git clone https://github.com/mfontanini/libtins
+
+echo -e "\ncd libtins\n"
+cd libtins
+
+echo -e "\nmkdir build\n"
+mkdir build
+
+echo -e "\nmkdir build\n"
+cd build
+
+echo -e "\ncmake ../ -DLIBTINS_ENABLE_CXX11=1\n"
+cmake ../ -DLIBTINS_ENABLE_CXX11=1
+
+echo -e "\nmake\n"
+make
+
+echo -e "\nsudo make install\n"
+sudo make install
+
+echo -e "\nsudo ldconfig\n"
+sudo ldconfig
+
+echo -e "\ncd ../../\n"
+cd ../../
+
+echo -e "\nrm -rf libtins\n"
+rm -rf libtins
+
+echo -e "\nlibtins installed successfully"
+
