8e5ae61a5e6179ad16cdd9465d710b6f5ac5e5ed
Troy Wolters <14ctw@queensu.ca>
2017-10-23 16:05:16 -0400

    Add client and server callback functions

--- TCPStream/segment.cpp
+++ TCPStream/segment.cpp
@@ -7,18 +7,64 @@ using namespace std;
 using Tins::TCPIP::Stream;
 using Tins::TCPIP::StreamFollower;
 
+// This will be called when there's new client data
+void on_client_data(Stream& stream) {
+
+    if (stream.is_partial_stream()) {
+        cout << "Skipping partial stream" << endl;
+        return;
+    }
+
+    // Get the client's payload, this is a vector<uint8_t>
+    const Stream::payload_type& payload = stream.client_payload();
+    int i;
+
+    cout << "Client data:" << endl;
+
+    for (i = 0; i < payload.size(); i++) {
+        cout << payload[i];
+    }
+
+    cout << endl;
+}
+
+// This will be called when there's new server data
+void on_server_data(Stream& stream) {
+
+    if (stream.is_partial_stream()) {
+        cout << "Skipping partial stream" << endl;
+        return;
+    }
+
+    const Stream::payload_type& payload = stream.client_payload();
+    int i;
+
+    cout << "Server data:" << endl;
+
+    for (i = 0; i < payload.size(); i++) {
+        cout << payload[i];
+    }
+
+    cout << endl;
+}
+
 // A stream closed properly
 void on_stream_closed(Stream& stream) {
-	cout << "Stream closed." << endl;
+    cout << "Stream from " << stream.client_addr_v4() << ":" << stream.client_port() << " to " << stream.server_addr_v4() << ":" << stream.server_port() << " closed" << endl;
 }
 
 // A stream was terminated. The second argument is the reason why it was terminated
 void on_stream_terminated(Stream& stream, StreamFollower::TerminationReason reason) {
-	cout << "Stream terminated." << endl;
+    cout << "Stream terminated." << endl;
 }
 
 // New stream is seen
 void on_new_stream(Stream& stream) {
-	stream.stream_closed_callback(&on_stream_closed);
-	cout << "New stream started." << endl;
+    cout << "New stream from " << stream.client_addr_v4() << ":" << stream.client_port() << " to " << stream.server_addr_v4() << ":" << stream.server_port() << endl;
+
+    stream.stream_closed_callback(&on_stream_closed);
+    stream.client_data_callback(&on_client_data);
+    stream.server_data_callback(&on_server_data);
 }
+
+
