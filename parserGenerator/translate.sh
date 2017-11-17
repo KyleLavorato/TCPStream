6659560a1e19bfdeb9b2864ca2ef72cc6c09633b
Kyle Lavorato <13kpl2@queensu.ca>
2017-11-17 14:48:30 -0500

    Updated help documentation on the translate.sh script to include the new -debug mode

--- parserGenerator/translate.sh
+++ parserGenerator/translate.sh
@@ -4,11 +4,12 @@ DIR=""
 CALLBACK=""
 
 if [[ "$1" == "-h" || $# > 2 ]]; then
-	printf "Usage:\t./translate.sh [DIRECTORY] [-nocallback]\n"
-	printf "\targuments are optional"
-	echo "DIRECTORY is an additional directory to move the output files to"
-	echo "This script will always copy generated files into ../testingGenerator"
-	echo "-nocallback tells the parser generator whether to include callback generation"
+	printf "Usage:\t./translate.sh [DIRECTORY] [-nocallback] [-debug]\n"
+	printf "\targuments are optional\n\n"
+	printf "DIRECTORY is an additional directory to move the output files to\n"
+	printf "This script will always copy generated parsers into ../TCPStream\n"
+	printf "[-nocallback] disables callback generation\n"
+	printf "[-debug] includes print statements for each field parsed\n"
 	exit 1
 fi
 
