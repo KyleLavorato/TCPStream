c1a7784158cab3f1c5d87364e1ba1bd3eaa6d003
Kyle Lavorato <13kpl2@queensu.ca>
2018-02-01 18:50:47 -0500

    Added TCPStream calls to testing; SPID does not run

--- TEST/ProcessStream.sh
+++ TEST/ProcessStream.sh
@@ -35,17 +35,17 @@ for j in PCAP/*; do
 	# Loop over all intermediates in folder
 	for i in `ls -v INTERMEDIATE/*.txt`; do
 		if grep -q "FTP" $i; then
-			printf "$COUNT, FTP\n" >> $tmpfile
+			printf "[$COUNT] protocol: FTP\n" >> $tmpfile
 		elif grep -q "FTP-DATA" $i; then
-			printf "$COUNT, FTP\n" >> $tmpfile
+			printf "[$COUNT] protocol: FTP\n" >> $tmpfile
 		elif grep -q "SMB" $i; then
-			printf "$COUNT, SMB\n" >> $tmpfile
+			printf "[$COUNT] protocol: SMB\n" >> $tmpfile
 		elif grep -q "HTTP" $i; then
-			printf "$COUNT, HTTP\n" >> $tmpfile
+			printf "[$COUNT] protocol: HTTP\n" >> $tmpfile
 		elif grep -q "TCP" $i; then
 			:
 		else
-			printf "$COUNT, FAILED\n" >> $tmpfile
+			printf "[$COUNT] protocol: FAILED\n" >> $tmpfile
 		fi
 		COUNT=$((COUNT+1))
 		rm $i
@@ -70,19 +70,20 @@ echo -en '\n[####                ] 20%'
 echo -en "\033[F\r"
 #sleep 2
 
-#cd ../DEMO/GENERATOR
-#./translate.sh -q
-#./interface.sh -q
-#cd ../
+cd ../DEMO/GENERATOR
+./makemake.sh -q
+./translate.sh -q
+./interface.sh -q
+cd ../
 
 echo -en "                               \r"
 echo -en "Compiling TCPStream\r"
 echo -en '\n[########            ] 40%'
 echo -en "\033[F\r"
 
-#make clean > /dev/null
-#make > /dev/null
-#cd ../TEST
+make clean > /dev/null
+make > /dev/null
+cd ../TEST
 
 ########## End Build ##########
 
@@ -93,16 +94,16 @@ echo -en "Running TCPStream on Pcaps\r"
 echo -en '\n[############        ] 60%'
 echo -en "\033[F\r"
 
-#rm -r ACTUAL_RESULT
-#mkdir ACTUAL_RESULT
+rm -r ACTUAL_RESULT
+mkdir ACTUAL_RESULT
 
-#for j in PCAP/*; do
-	#../DEMO/shitty_wireshark -h > /dev/null
-	#filename=`basename $j .pcapng`
-	#filename=`basename $filename .pcap`
-	#filename=`basename $filename .cap`
-	#./shitty_wireshark > ACTUAL_RESULT/$filename.txt
-#done
+for j in PCAP/*; do
+	filename=`basename $j .pcapng`
+	filename=`basename $filename .pcap`
+	filename=`basename $filename .cap`
+	../DEMO/TCPStream file string-matching ../DEMO/configs/string-matching-config.txt $j > ACTUAL_RESULT/$filename_STRING.txt
+	../DEMO/TCPStream file spid ../DEMO/configs/spid-config-testing.txt $j > ACTUAL_RESULT/$filename_SPID.txt
+done
 
 ########## End Processing ##########
 
@@ -116,14 +117,24 @@ echo -en "\033[F\r"
 
 # Find number of errors between actual and expected
 ERRORS=0
+ERRORM=0
 tmpfile=$(mktemp /tmp/ProcessStream.XXXXXX)
-for j in ACTUAL_RESULT/*.txt; do
-	RESULT=$(diff $j EXPECTED_RESULT/`basename $j` | grep "^>" | wc -l)
+for j in EXPECTED_RESULT/*.txt; do
+	## Test SPID
+	RESULT=$(diff $j ACTUAL_RESULT/`basename $j .txt`_SPID.txt | grep "^>" | wc -l)
 	if (( $RESULT > 0 )); then
-		echo -e "Mismatch in `basename $j .txt`\n" >>$tmpfile
-		diff $j EXPECTED_RESULT/`basename $j` >> $tmpfile
+		echo -e "Mismatch in `basename $j .txt`_SPID\n" >> $tmpfile
+		diff $j ACTUAL_RESULT/`basename $j .txt`_SPID.txt >> $tmpfile
 	fi
 	ERRORS=$((ERRORS+RESULT))
+	
+	## Test String-Matching
+	RESULT=$(diff $j ACTUAL_RESULT/`basename $j .txt`_STRING.txt | grep "^>" | wc -l)
+	if (( $RESULT > 0 )); then
+		echo -e "Mismatch in `basename $j .txt`_STRING\n" >> $tmpfile
+		diff $j ACTUAL_RESULT/`basename $j .txt`_STRING.txt >> $tmpfile
+	fi
+	ERRORM=$((ERRORM+RESULT))
 done
 touch Mismatch.txt
 rm Mismatch.txt
@@ -138,18 +149,27 @@ for j in EXPECTED_RESULT/*.txt; do
 done
 
 # Find percent failed
-PERCENT=$(awk "BEGIN { pc=100*${ERRORS}/${ATTEMPTS}; i=int(pc); print (pc) }")
+PERCENTS=$(awk "BEGIN { pc=100*${ERRORS}/${ATTEMPTS}; i=int(pc); print (pc) }")
+PERCENTM=$(awk "BEGIN { pc=100*${ERRORM}/${ATTEMPTS}; i=int(pc); print (pc) }")
 
 # Find successes
-SUCCESS=$((ATTEMPTS-FAILED))
+SUCCESS=$((ATTEMPTS-ERRORS))
+SUCCESM=$((ATTEMPTS-ERRORM))
 
 echo -en "                               \r"
 echo -en "Complete | Results Below\r"
 echo -en '\n[####################] 100%'
 echo -en '\n\n'
+echo "SPID Results"
 echo "Successful Matches: $SUCCESS"
 echo "Identifications Failed: $ERRORS"
 echo "Total Attempts: $ATTEMPTS"
-printf "Failure Rate: %0.2f%%\n" $PERCENT
+printf "Failure Rate: %0.2f%%\n" $PERCENTS
+echo -en "\n\nString-Matching Results\n"
+echo "Successful Matches: $SUCCESM"
+echo "Identifications Failed: $ERRORM"
+echo "Total Attempts: $ATTEMPTS"
+printf "Failure Rate: %0.2f%%\n" $PERCENTM
+echo -en "\n\nMismatches printed to Mismatch.txt\n"
 
 ########## End Results ##########
